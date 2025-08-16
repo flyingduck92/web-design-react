c8739481863e48fe3d6d01c2411a09059a099eb9
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-16 11:54:42 +0700

    Add hero welcome heading title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,9 @@ import hero from '../assets/pexels-sebastian-dziomba-762357063-30397052_hero.jpg
 
 function Hero() {
   return (
-    <section>
-      <picture>
+    <section className='hero'>
+      <h1>Welcome to Exhibition Museum of Discovery</h1>
+      <picture className='hero-img'>
         <source media='(width < 800px)' srcSet={hero} />
         <source media='(width >= 800px)' srcSet={hero} />
         <img src={hero} alt="Exhibition hero" />

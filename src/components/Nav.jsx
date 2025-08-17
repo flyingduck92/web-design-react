3dce0d92b2414c77c3a8ecc2785b1ffa952808c8
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-17 10:59:20 +0700

    Change Profile nav to Background nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,9 @@ function Nav() {
       <div id='logo'><a href="#">Exhibition Museum of Discovery </a></div>
       <ul id='menus'>
         <li><a href="#">Home</a></li>
-        <li><a href="#">Profile</a></li>
         <li><a href="#">News</a></li>
         <li><a href="#">Schedule</a></li>
+        <li><a href="#">Background</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
     </nav>
